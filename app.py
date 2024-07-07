67c34bc766626bfa3c3f6d57ee49985900943c15
AngelesNav <angeles.navarrete@mail.udp.cl>
2024-07-07 12:23:51 -0400

    update

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, request, send_from_directory, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_migrate import Migrate
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -15,7 +18,7 @@ migrate = Migrate(app, db)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=True)
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
 
@@ -37,18 +40,64 @@ class Favorite(db.Model):
     user = db.relationship('User', backref=db.backref('favorites', lazy=True))
     file = db.relationship('File', backref=db.backref('favorites', lazy=True))
 
+@app.before_request
+def log_request_info():
+    app.logger.debug('Headers: %s', request.headers)
+    app.logger.debug('Body: %s', request.get_data())
+
 @app.route('/users/login', methods=['POST'])
 def authenticate_user():
     data = request.json
     email = data['email']
     password = data['password']
-    user = User.query.filter_by(email=email, password=password).first()
-    if user:
+    user = User.query.filter_by(email=email).first()
+    if user and check_password_hash(user.password, password):
         return jsonify({"id": user.id, "first_name": user.first_name, "last_name": user.last_name})
-    else:
-        return jsonify({"error": "Invalid email or password"}), 401
+    return jsonify({"error": "Invalid email or password"}), 401
 
-@app.route('/files', methods=['POST'])
+@app.route('/users/google-login', methods=['POST'])
+def google_login():
+    data = request.json
+    token = data.get('token')
+    try:
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(), "367342764631-tejhut1n7n3mls174d2ffmq86cdmf1ui.apps.googleusercontent.com")
+
+        email = idinfo['email']
+        first_name = idinfo['given_name']
+        last_name = idinfo['family_name']
+
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            user = User(email=email, first_name=first_name, last_name=last_name)
+            db.session.add(user)
+            db.session.commit()
+
+        return jsonify({"id": user.id, "first_name": user.first_name, "last_name": user.last_name})
+    except ValueError:
+        return jsonify({"error": "Invalid token"}), 400
+
+@app.route('/users/register', methods=['POST'])
+def register_user():
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+    first_name = data.get('firstName')
+    last_name = data.get('lastName')
+    
+    if not email or not password or not first_name or not last_name:
+        return jsonify({"error": "Missing fields"}), 400
+    
+    if User.query.filter_by(email=email).first():
+        return jsonify({"error": "Email already in use"}), 400
+    
+    hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+    new_user = User(email=email, password=hashed_password, first_name=first_name, last_name=last_name)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return jsonify({"message": "User registered successfully"}), 201
+
+@app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
         return jsonify({"error": "No file part"}), 400
@@ -56,27 +105,22 @@ def upload_file():
     if file.filename == '':
         return jsonify({"error": "No selected file"}), 400
     if file:
-        user_id = request.form['user_id']
-        title = request.form['title']
-        description = request.form['description']
-        keywords = request.form['keywords']
-        course = request.form['course']
-        file_type = file.filename.split('.')[-1].lower()
         filename = file.filename
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(filepath)
-        new_file = File(filename=filename, user_id=user_id, title=title, description=description, keywords=keywords, course=course, file_type=file_type)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        title = request.form.get('title')
+        description = request.form.get('description')
+        keywords = request.form.get('keywords')
+        course = request.form.get('course')
+        file_type = request.form.get('file_type')
+        user_id = request.form.get('user_id')
+        new_file = File(filename=filename, title=title, description=description, keywords=keywords, course=course, file_type=file_type, user_id=user_id)
         db.session.add(new_file)
         db.session.commit()
-        return jsonify({"id": new_file.id, "filename": new_file.filename})
+        return jsonify({"message": "File uploaded successfully"}), 201
 
 @app.route('/files', methods=['GET'])
 def get_files():
-    course = request.args.get('course', None)
-    if course:
-        files = File.query.filter_by(course=course).all()
-    else:
-        files = File.query.all()
+    files = File.query.all()
     file_list = [{"id": file.id, "filename": file.filename, "title": file.title, "description": file.description, "keywords": file.keywords, "course": file.course, "file_type": file.file_type, "user_id": file.user_id} for file in files]
     return jsonify({"files": file_list})
 
@@ -114,17 +158,7 @@ def get_user_files(user_id):
     file_list = [{"id": file.id, "filename": file.filename, "title": file.title, "description": file.description, "keywords": file.keywords, "course": file.course, "file_type": file.file_type, "user_id": file.user_id} for file in files]
     return jsonify({"files": file_list})
 
-def add_default_users():
-    if not User.query.filter_by(email="angeles@mail.com").first():
-        db.session.add(User(email="angeles@mail.com", password="1234", first_name="Angeles", last_name="Navarrete"))
-    if not User.query.filter_by(email="simon@mail.com").first():
-        db.session.add(User(email="simon@mail.com", password="1234", first_name="Simon", last_name="Mujica"))
-    if not User.query.filter_by(email="diego@mail.com").first():
-        db.session.add(User(email="diego@mail.com", password="1234", first_name="Diego", last_name="Hernandez"))
-    db.session.commit()
-
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        add_default_users()
     app.run(debug=True)
